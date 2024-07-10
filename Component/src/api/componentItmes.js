1b90cfa5d7f5398f81ac0a062be88f43a1aabb93
yeji.jin <dpwl322@naver.com>
2024-07-10 10:23:27 +0900

    ssh test

--- Component/src/api/componentItmes.js
+++ Component/src/api/componentItmes.js
@@ -1,63 +1,64 @@
 const componentItmes = [
+  //ssh test
   {
-    componentNames: "Button",
+    componentNames: 'Button',
     items: [
-      { title: "button1-1" },
-      { title: "button1-2" },
-      { title: "button1-3" },
-      { title: "button1-4" },
-      { title: "button1-5" },
-      { title: "button1-6" },
-      { title: "button1-7" },
-      { title: "button1-8" },
-      { title: "button1-9" },
-      { title: "button1-10" },
-    ],
+      { title: 'button1-1' },
+      { title: 'button1-2' },
+      { title: 'button1-3' },
+      { title: 'button1-4' },
+      { title: 'button1-5' },
+      { title: 'button1-6' },
+      { title: 'button1-7' },
+      { title: 'button1-8' },
+      { title: 'button1-9' },
+      { title: 'button1-10' }
+    ]
   },
   {
-    componentNames: "List",
+    componentNames: 'List',
     items: [
-      { title: "List-1" },
-      { title: "List-2" },
-      { title: "List-3" },
-      { title: "List-4" },
-      { title: "List-5" },
-      { title: "List-6" },
-      { title: "List-7" },
-      { title: "List-8" },
-      { title: "List-9" },
-      { title: "List-10" },
-    ],
+      { title: 'List-1' },
+      { title: 'List-2' },
+      { title: 'List-3' },
+      { title: 'List-4' },
+      { title: 'List-5' },
+      { title: 'List-6' },
+      { title: 'List-7' },
+      { title: 'List-8' },
+      { title: 'List-9' },
+      { title: 'List-10' }
+    ]
   },
   {
-    componentNames: "Input",
+    componentNames: 'Input',
     items: [
-      { title: "Input-1" },
-      { title: "Input-2" },
-      { title: "Input-3" },
-      { title: "Input-4" },
-      { title: "Input-5" },
-      { title: "Input-6" },
-      { title: "Input-7" },
-      { title: "Input-8" },
-      { title: "Input-9" },
-      { title: "Input-10" },
-    ],
+      { title: 'Input-1' },
+      { title: 'Input-2' },
+      { title: 'Input-3' },
+      { title: 'Input-4' },
+      { title: 'Input-5' },
+      { title: 'Input-6' },
+      { title: 'Input-7' },
+      { title: 'Input-8' },
+      { title: 'Input-9' },
+      { title: 'Input-10' }
+    ]
   },
   {
-    componentNames: "Slide",
+    componentNames: 'Slide',
     items: [
-      { title: "Slide-1" },
-      { title: "Slide-2" },
-      { title: "Slide-3" },
-      { title: "Slide-4" },
-      { title: "Slide-5" },
-      { title: "Slide-6" },
-      { title: "Slide-7" },
-      { title: "Slide-8" },
-      { title: "Slide-9" },
-      { title: "Slide-10" },
-    ],
-  },
+      { title: 'Slide-1' },
+      { title: 'Slide-2' },
+      { title: 'Slide-3' },
+      { title: 'Slide-4' },
+      { title: 'Slide-5' },
+      { title: 'Slide-6' },
+      { title: 'Slide-7' },
+      { title: 'Slide-8' },
+      { title: 'Slide-9' },
+      { title: 'Slide-10' }
+    ]
+  }
 ];
 export default componentItmes;
