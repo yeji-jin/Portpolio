04fff11ada6eb2c50a6b756a0001a75c11369a95
yeji.jin <dpwl322@naver.com>
2024-11-04 11:21:28 +0900

    Revert "test"
    This reverts commit a28eff7ebd4bd3381f1bb53e66ed09a523d37e43.

--- Component/src/App.js
+++ Component/src/App.js
@@ -13,7 +13,7 @@ const ComponentMain = styled.div`
   box-shadow: 0 0.5px 0 1px rgba(255, 255, 255, 0.23) inset,
     0 1px 0 0 rgba(255, 255, 255, 0.66) inset, 0 4px 16px rgba(0, 0, 0, 0.12);
 `;
- 
+
 //NavList
 const NavList = [
   {
@@ -23,7 +23,7 @@ const NavList = [
   {
     title: "nav-2",
     href: "/nav",
-  }, 
+  },
   {
     title: "nav-3",
     href: "/nav",
